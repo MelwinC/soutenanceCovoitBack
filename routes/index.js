1c76b4195a1d53bbfad5cb0102bea2c2f9d8f2b6
MelwinC <contact@melwinchenu.fr>
2024-02-22 23:01:20 +0100

    disable temporairement l'index des routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const router = express.Router();
-const villeRoutes = require('./ville');
-
-router.use('/villes', villeRoutes);
-// Utilisez router.use pour monter d'autres routes sur des chemins spécifiques si nécessaire
-
-module.exports = router;
+// const express = require('express');
+// const router = express.Router();
+// const villeRoutes = require('./ville');
+//
+// router.use('/villes', villeRoutes);
+// // Utilisez router.use pour monter d'autres routes sur des chemins spécifiques si nécessaire
+//
+// module.exports = router;
