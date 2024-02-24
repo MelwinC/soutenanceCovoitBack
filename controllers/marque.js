1cc095e840dfde1cc3f4ac7bbaa9158a9d176b37
MelwinC <contact@melwinchenu.fr>
2024-02-24 16:26:03 +0100

    migrate -> async / await

--- controllers/marque.js
+++ controllers/marque.js
@@ -1,52 +1,36 @@
 const models = require("../models");
 const Marque = models.marque;
 
-exports.readAll = (req, res) => {
-    Marque.findAll()
-        .then(marques => {
-            if(marques.length === 0){
-                res.status(404).send({ message: "NOK" });
-            } else {
-                const data = marques.map(marque => {
-                    return {
-                        id: marque.id,
-                        nom: marque.nom,
-                    }
-                })
-                res.send({ message: "OK", marques: data });
+exports.readAll = async (req, res) => {
+    try {
+        const marques = await Marque.findAll();
+        if(marques.length === 0){
+            return res.status(404).send({ message: "NOK" });
+        }
+        const data = marques.map(marque => {
+            return {
+                id: marque.id,
+                nom: marque.nom,
             }
-        })
-        .catch(err => {
-            res.status(500).send({ message: err.message });
         });
+        res.status(200).send({ message: "OK", marques: data });
+    } catch (error) {
+        res.status(500).send({ message: "NOK" });
+    }
 };
 
-exports.insert = (req, res) => {
-    if(!req.body.nom){
-        res.status(400).send({ message: "NOK" });
-    } else {
-        Marque.findAll({
-            where: {
-                nom: req.body.nom
-            }
-        })
-            .then(marque => {
-                if (marque.length > 0) {
-                    res.status(400).send({ message: "NOK" });
-                } else {
-                    Marque.create({
-                        nom: req.body.nom,
-                    })
-                        .then(() => {
-                            res.send({ message: "OK" });
-                        })
-                        .catch(err => {
-                            res.status(500).send({ message: err.message });
-                        });
-                }
-            })
-            .catch(err => {
-                res.status(500).send({ message: err.message });
-            });
+exports.insert = async (req, res) => {
+    try {
+        if(!req.body.nom){
+            return res.status(400).send({ message: "NOK" });
+        }
+        const marque = await Marque.findOne({ where: { nom: req.body.nom }});
+        if(marque){
+            return res.status(400).send({ message: "NOK" });
+        }
+        await Marque.create({ nom: req.body.nom});
+        res.status(201).send({ message: "OK" });
+    } catch (error) {
+        res.status(500).send({ message: "NOK" });
     }
 };
