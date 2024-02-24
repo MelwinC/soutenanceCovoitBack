5ecff6d65c8e1156896df600a24e1f6b40270168
MelwinC <contact@melwinchenu.fr>
2024-02-24 22:51:37 +0100

    fix next() missing so request expires

--- middlewares/verifyRegister.js
+++ middlewares/verifyRegister.js
@@ -1,5 +1,6 @@
 const models = require("../models");
 const Compte = models.compte;
+const Personne = models.personne
 
 checkDuplicateLogin = async (req, res, next) => {
     try{
@@ -14,13 +15,34 @@ checkDuplicateLogin = async (req, res, next) => {
                 message: "Le login est déjà attribué !"
             });
         }
+        next();
+    } catch (error) {
+        res.status(500).send({ message: "NOK" });
+    }
+};
+
+checkDuplicateEmail = async (req, res, next) => {
+    try{
+        if(!req.body.email){
+            return res.status(400).send({
+                message: "Veuillez remplir tous les champs !"
+            });
+        }
+        const personne = await Personne.findOne({ where: { email: req.body.email } });
+        if(personne){
+            return res.status(400).send({
+                message: "L'email est déjà attribué !"
+            });
+        }
+        next();
     } catch (error) {
         res.status(500).send({ message: "NOK" });
     }
 };
 
 const verifyRegister = {
-    checkDuplicateLogin: checkDuplicateLogin
+    checkDuplicateLogin: checkDuplicateLogin,
+    checkDuplicateEmail: checkDuplicateEmail
 };
 
 module.exports = verifyRegister;
