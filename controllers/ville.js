66d697b8cafedb0441035b0d02344f98cc582072
MelwinC <contact@melwinchenu.fr>
2024-02-24 16:26:33 +0100

    missed a return

--- controllers/ville.js
+++ controllers/ville.js
@@ -4,7 +4,7 @@ const Ville = models.ville;
 exports.insert = async (req, res) => {
     try {
         if(!req.body.ville || !req.body.cp){
-            res.status(400).send({ message: "NOK" });
+            return res.status(400).send({ message: "NOK" });
         }
 
         const villeExists = await Ville.findOne({ where: { ville: req.body.ville, cp: req.body.cp }});
