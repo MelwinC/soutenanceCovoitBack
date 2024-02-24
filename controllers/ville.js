53d69284a585341b1c057b6d797ef78a7aab9a05
MelwinC <contact@melwinchenu.fr>
2024-02-24 15:55:15 +0100

    migrate to async await

--- controllers/ville.js
+++ controllers/ville.js
@@ -1,54 +1,40 @@
 const models = require("../models");
 const Ville = models.ville;
 
-exports.insert = (req, res) => {
-    if(!req.body.ville || !req.body.cp){
-        res.status(400).send({ message: "NOK" });
-    } else {
-        Ville.findAll({
-            where: {
-                ville: req.body.ville
-            }
-        })
-            .then(ville => {
-                if (ville.length > 0) {
-                    res.status(400).send({ message: "NOK" });
-                } else {
-                    Ville.create({
-                        ville: req.body.ville,
-                        cp: req.body.cp
-                    })
-                        .then(() => {
-                            res.send({ message: "OK" });
-                        })
-                        .catch(err => {
-                            res.status(500).send({ message: err.message });
-                        });
-                }
-            })
-            .catch(err => {
-                res.status(500).send({ message: err.message });
-            });
+exports.insert = async (req, res) => {
+    try {
+        if(!req.body.ville || !req.body.cp){
+            res.status(400).send({ message: "NOK" });
+        }
+
+        const villeExists = await Ville.findOne({ where: { ville: req.body.ville, cp: req.body.cp }});
+        if(villeExists){
+            return res.status(404).send({ message: "NOK" });
+        }
+
+        await Ville.create({ ville: req.body.ville, cp: req.body.cp });
+        res.status(201).send({ message: "OK" });
+    } catch (error) {
+        res.status(500).send({ message: "NOK" });
     }
 };
 
-exports.readAll = (req, res) => {
-    Ville.findAll()
-        .then(villes => {
-            if(villes.length === 0){
-                res.status(404).send({ message: "NOK" });
-            } else {
-                const data = villes.map(ville => {
-                    return {
-                        id: ville.id,
-                        ville: ville.ville,
-                        cp: ville.cp
-                    }
-                })
-                res.send({ message: "OK", villes: data });
+exports.readAll = async (req, res) => {
+    try {
+        const villes = await Ville.findAll();
+        if(villes.length === 0) {
+            return res.status(404).send({message: "NOK"});
+        }
+
+        const data = villes.map(ville => {
+            return {
+                id: ville.id,
+                ville: ville.ville,
+                cp: ville.cp
             }
-        })
-        .catch(err => {
-            res.status(500).send({ message: err.message });
         });
+        res.status(200).send({ message: "OK", villes: data });
+    } catch (error) {
+        res.status(500).send({ message: "NOK" });
+    }
 };
