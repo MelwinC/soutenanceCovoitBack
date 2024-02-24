18cf88aeb16a35bd182a47ac4166d55391476cad
MelwinC <contact@melwinchenu.fr>
2024-02-24 04:00:50 +0100

    then() -> async / await  & apply roles changes

--- middlewares/verifyRegister.js
+++ middlewares/verifyRegister.js
@@ -1,47 +1,26 @@
 const models = require("../models");
-const roles = ["utilisateur", "personne", "admin"];
 const Compte = models.compte;
 
-checkDuplicateLogin = (req, res, next) => {
-    //! find by login
-    if(!req.body.login){
-        res.status(400).send({
-            message: "Veuillez remplir tous les champs !"
-        });
-    } else {
-        Compte.findOne({
-            where: {
-                login: req.body.login
-            }
-        }).then(compte => {
-            if (compte) {
-                res.status(400).send({
-                    message: "Le login est déjà attribué !"
-                });
-                return;
-            }
-            next();
-        });
-    }
-};
-
-checkRolesExisted = (req, res, next) => {
-    if (req.body.roles) {
-        for (let i = 0; i < req.body.roles.length; i++) {
-            if (!roles.includes(req.body.roles[i])) {
-                res.status(400).send({
-                    message: "Le role n'existe pas = " + req.body.roles[i]
-                });
-                return;
-            }
+checkDuplicateLogin = async (req, res, next) => {
+    try{
+        if(!req.body.login){
+            return res.status(400).send({
+                message: "Veuillez remplir tous les champs !"
+            });
         }
+        const compte = await Compte.findOne({ where: { login: req.body.login } });
+        if(compte){
+            return res.status(400).send({
+                message: "Le login est déjà attribué !"
+            });
+        }
+    } catch (error) {
+        res.status(500).send({ message: "NOK" });
     }
-    next();
 };
 
 const verifyRegister = {
-    checkDuplicateLogin: checkDuplicateLogin,
-    checkRolesExisted: checkRolesExisted
+    checkDuplicateLogin: checkDuplicateLogin
 };
 
 module.exports = verifyRegister;
