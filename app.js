1cf06b6025ae3ecdf8b163a980c4df7f149cccd4
MelwinC <contact@melwinchenu.fr>
2024-03-19 11:04:23 +0100

    update cors origin to authorize the frontend

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 const cors = require("cors");
 const { specs, swaggerUi } = require("./swagger");
-const routes = require('./routes');
+const routes = require("./routes");
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:8080"
+  origin: "http://localhost:3000",
 };
 
-app.use(cors(corsOptions)).use(logger('dev')).use(express.json()).use(express.urlencoded({ extended: true })).use(cookieParser()).use(express.static(path.join(__dirname, 'public')));
-app.use('/', routes).use("/doc", swaggerUi.serve, swaggerUi.setup(specs));
+app
+  .use(cors(corsOptions))
+  .use(logger("dev"))
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .use(cookieParser())
+  .use(express.static(path.join(__dirname, "public")));
+app.use("/", routes).use("/doc", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use((req, res, next) => {
-  res.status(404).send({message: "Route not found"});
+  res.status(404).send({ message: "Route not found" });
 });
 app.use((err, req, res, next) => {
-  console.log(err)
+  console.log(err);
   res.status(500).send({ message: "Server error" });
 });
 
